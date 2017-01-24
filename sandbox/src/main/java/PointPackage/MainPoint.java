acc305f80047d6d1cdcf22dd69b34827fd669542
DEX-6 <irina14.91@bk.ru>
2017-01-24 23:29:09 +0300

    Задача 2. Исправления

--- sandbox/src/main/java/PointPackage/MainPoint.java
+++ sandbox/src/main/java/PointPackage/MainPoint.java
@@ -5,39 +5,35 @@ package PointPackage;
  */
 public class MainPoint {
     public static void main(String[] args) {
-        Point p1 = new Point();
-        Point p2 = new Point();
 
         {
-            p1.setCoordinates(1.1, 1.1);
-            p2.setCoordinates(1.2, 1.2);
+            Point p1 = new Point(1.1, 1.1);
+            Point p2 = new Point(1.2, 1.2);
 
-            System.out.println("x1 = " + p1.getX() + "; " + "y1 = " + p1.getY());
-            System.out.println("x1 = " + p2.getX() + "; " + "y1 = " + p2.getY());
+            System.out.println("x1 = " + p1.x + "; " + "y1 = " + p1.y);
+            System.out.println("x2 = " + p2.x + "; " + "y2 = " + p2.y);
             System.out.println("Расстояние между двумя точками равно: " + Point.distance(p1, p2));
             System.out.println();
         }
 
         {
-            p1.setCoordinates(0.1, 0.1);
-            p2.setCoordinates(0.2, 0.2);
+            Point p3 = new Point(-1.1, -1.1);
+            Point p4 = new Point(-1.2, -1.2);
 
-            System.out.println("x1 = " + p1.getX() + "; " + "y1 = " + p1.getY());
-            System.out.println("x1 = " + p2.getX() + "; " + "y1 = " + p2.getY());
-            System.out.println("Расстояние между двумя точками равно: " + Point.distance(p1, p2));
+            System.out.println("x3 = " + p3.x + "; " + "y3 = " + p3.y);
+            System.out.println("x4 = " + p4.x + "; " + "y4 = " + p4.y);
+            System.out.println("Расстояние между двумя точками равно: " + Point.distance(p3, p4));
             System.out.println();
         }
 
         {
-            p1.setCoordinates(2, 2);
-            p2.setCoordinates(3, 3);
+            Point p5 = new Point(2, 2);
+            Point p6 = new Point(3, 3);
 
-
-            System.out.println("x1 = " + p1.getX() + "; " + "y1 = " + p1.getY());
-            System.out.println("x1 = " + p2.getX() + "; " + "y1 = " + p2.getY());
-            System.out.println("Расстояние между двумя точками равно: " + Point.distance(p1, p2));
+            System.out.println("x5 = " + p5.x + "; " + "y5 = " + p5.y);
+            System.out.println("x6 = " + p6.x + "; " + "y6 = " + p6.y);
+            System.out.println("Расстояние между двумя точками равно: " + Point.distance(p5, p6));
             System.out.println();
         }
-
     }
 }
--- sandbox/src/main/java/PointPackage/Point.java
+++ sandbox/src/main/java/PointPackage/Point.java
@@ -6,20 +6,12 @@ import static java.lang.Math.pow;
 import static java.lang.Math.sqrt;
 
 public class Point {
-    double x;
-    double y;
+    public double x;
+    public double y;
 
-    void setCoordinates(double x, double y) {
+    public Point (double x, double y){
         this.x = x;
         this.y = y;
-    }
-
-    double getX() {
-        return x;
-    }
-
-    double getY() {
-        return y;
     }
 
     public static double distance(Point p1, Point p2) {
