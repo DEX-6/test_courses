e97fde1e31befd547b7c955d2d0ec37ea0d1d8f2
DEX-6 <irina14.91@bk.ru>
2017-02-09 21:55:31 +0300

    Приведение классов теста к двухуровневой архитектуре

--- addressbook-web-tests/src/test/java/ru/stqa/test_courses/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/test_courses/addressbook/tests/ContactCreationTests.java
@@ -1,4 +1,4 @@
-package ru.stqa.test_courses.addressbook;
+package ru.stqa.test_courses.addressbook.tests;
 
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
@@ -8,6 +8,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.*;
+import ru.stqa.test_courses.addressbook.model.ContactData;
 
 public class ContactCreationTests {
     FirefoxDriver wd;
