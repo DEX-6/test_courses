ed9cce1553fec242ce27d2dd55272fe1080b3746
DEX-6 <irina14.91@bk.ru>
2017-02-18 00:37:25 +0300

    добавление нового класса-помощника

---
+++ addressbook-web-tests/src/test/java/ru/stqa/test_courses/addressbook/appmeneger/ContactHelper.java
@@ -0,0 +1,12 @@
+package ru.stqa.test_courses.addressbook.appmeneger;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by i-ru on 18.02.2017.
+ */
+public class ContactHelper extends HelperBase {
+    public ContactHelper(FirefoxDriver wd) {
+        super(wd);
+    }
+}
