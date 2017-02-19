a32a939f43f714eddba063951d33629ee1fce2b7
DEX-6 <irina14.91@bk.ru>
2017-02-20 01:00:41 +0300

    метод модификации группы (без рекордера)

---
+++ addressbook-web-tests/src/test/java/ru/stqa/test_courses/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,19 @@
+package ru.stqa.test_courses.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.test_courses.addressbook.model.GroupData;
+
+/**
+ * Created by i-ru on 19.02.2017.
+ */
+public class GroupModificationTests extends TestBase {
+    @Test
+    public void testGroupModification() {
+        app.getNavigationHelper().goToGroupPage();
+        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().initGroupModification();
+        app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+        app.getGroupHelper().submitGroupModification();
+        app.getGroupHelper().returnToGroupPage();
+    }
+}
