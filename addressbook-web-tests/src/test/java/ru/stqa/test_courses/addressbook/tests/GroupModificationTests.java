de584e17d9b45c1a30b987eef76432a7de5da1a3
DEX-6 <irina14.91@bk.ru>
2017-05-02 07:17:43 +0300

    Улучшение внешнего вида проверок (группы)
    Hamcrest

--- addressbook-web-tests/src/test/java/ru/stqa/test_courses/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/test_courses/addressbook/tests/GroupModificationTests.java
@@ -1,12 +1,13 @@
 package ru.stqa.test_courses.addressbook.tests;
 
-import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.test_courses.addressbook.model.GroupData;
+import ru.stqa.test_courses.addressbook.model.Groups;
 
-import java.util.Comparator;
-import java.util.Set;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.testng.Assert.assertEquals;
 
 /**
  * Created by i-ru on 19.02.2017.
@@ -23,19 +24,14 @@ public class GroupModificationTests extends TestBase {
 
     @Test
     public void testGroupModification() {
-        Set<GroupData> before = app.group().all();
+        Groups before = app.group().all();
         GroupData modifiedGroup = before.iterator().next();
         GroupData group = new GroupData()
                 .withId(modifiedGroup.getId()).withName("test1").withHeader("test2").withFooter("test3");
         app.group().modify(group);
-        Set<GroupData> after = app.group().all();
-        Assert.assertEquals(after.size(), before.size());
-
-        before.remove(modifiedGroup);
-        before.add(group);
-
-        Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
-        Assert.assertEquals(before, after);
+        Groups after = app.group().all();
+        assertEquals(after.size(), before.size());
+        assertThat(after, equalTo(before.without(modifiedGroup).withAdded(group)));
     }
 
 
