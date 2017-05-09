c529d12f8a9e9f55812c408eabcfa8c6de0f3269
DEX-6 <irina14.91@bk.ru>
2017-05-09 21:47:06 +0300

    поправка к негативному тесту (удаление групп)

--- addressbook-web-tests/src/test/java/ru/stqa/test_courses/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/test_courses/addressbook/tests/GroupCreationTests.java
@@ -25,7 +25,7 @@ public class GroupCreationTests extends TestBase {
     public void testGroupBadCreation() {
         app.goTo().groupPage();
         Groups before = app.group().all();
-        GroupData group = new GroupData().withName("test");
+        GroupData group = new GroupData().withName("test'");
         app.group().create(group);
         assertThat(app.group().count(), equalTo(before.size()));
         Groups after = app.group().all();
