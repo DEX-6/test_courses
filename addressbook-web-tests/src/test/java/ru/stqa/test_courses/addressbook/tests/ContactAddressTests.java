835e419c8a692ff16e0b7033e8ab942af04430c1
DEX-6 <irina14.91@bk.ru>
2017-05-10 07:22:41 +0300

    some refactoring

--- addressbook-web-tests/src/test/java/ru/stqa/test_courses/addressbook/tests/ContactAddressTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/test_courses/addressbook/tests/ContactAddressTests.java
@@ -12,7 +12,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class ContactAddressTests extends TestBase {
 
     @Test
-    public void testContactPhone() {
+    public void testContactAddress() {
         app.goTo().homePage();
         ContactData contact = app.contact().all().iterator().next();
         ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
