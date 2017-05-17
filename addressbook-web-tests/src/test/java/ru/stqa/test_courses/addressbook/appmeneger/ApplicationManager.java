0e6c339f9e9542bbd766d585106a99553bdc3420
DEX-6 <irina14.91@bk.ru>
2017-05-17 23:13:31 +0300

    Модификация групп (БД)

--- addressbook-web-tests/src/test/java/ru/stqa/test_courses/addressbook/appmeneger/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/test_courses/addressbook/appmeneger/ApplicationManager.java
@@ -25,6 +25,7 @@ public class ApplicationManager {
     private ContactHelper contactHelper;
     private String browser;
     private final Properties properties;
+    private DbHelper dbHelper;
 
     public ApplicationManager(String browser) {
         this.browser = browser;
@@ -34,6 +35,9 @@ public class ApplicationManager {
     public void init() throws IOException {
         String target = System.getProperty("target", "local");
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
+        dbHelper = new DbHelper();
+
         if (browser.equals(BrowserType.FIREFOX)) {
             wd = new FirefoxDriver();
         } else if (browser.equals(BrowserType.CHROME)) {
@@ -68,4 +72,9 @@ public class ApplicationManager {
     public ContactHelper contact() {
         return contactHelper;
     }
+
+    public DbHelper db(){
+        return dbHelper;
+    }
+
 }
---
+++ addressbook-web-tests/src/test/java/ru/stqa/test_courses/addressbook/appmeneger/DbHelper.java
@@ -0,0 +1,36 @@
+package ru.stqa.test_courses.addressbook.appmeneger;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import ru.stqa.test_courses.addressbook.model.GroupData;
+import ru.stqa.test_courses.addressbook.model.Groups;
+
+import java.util.List;
+
+/**
+ * Created by i-ru on 17.05.2017.
+ */
+public class DbHelper {
+    private final SessionFactory sessionFactory;
+
+    public DbHelper() {
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure() // configures settings from hibernate.cfg.xml
+                .build();
+
+        sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+    }
+
+    public Groups groups() {
+
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        List<GroupData> result = session.createQuery("from GroupData").list();
+        session.getTransaction().commit();
+        session.close();
+        return  new Groups(result);
+    }
+}
