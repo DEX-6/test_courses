2c83a61d97ecec3e87f297cc16c52584143dbc84
DEX-6 <irina14.91@bk.ru>
2017-05-18 00:13:56 +0300

    Модификация контактов (БД)

--- addressbook-web-tests/src/test/java/ru/stqa/test_courses/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/test_courses/addressbook/tests/ContactModificationTests.java
@@ -16,8 +16,8 @@ public class ContactModificationTests extends TestBase {
 
     @BeforeMethod
     public void ensurePreconditions() {
-        app.goTo().homePage();
-        if (app.contact().all().size() == 0) {
+        if (app.db().contacts().size() == 0) {
+            app.goTo().homePage();
             app.contact().create(new ContactData().withName("Денис").withMiddleName("Станиславович").withLastName("Воронцов")
                     .withNickName("DEX-6").withCompany("Космический Мозгоед").withAddress("планета Земля")
                     .withPhone("+79856405255").withEmail("dex-6@mail.ru").withGroup("test1"));
@@ -26,14 +26,15 @@ public class ContactModificationTests extends TestBase {
 
     @Test
     public void testContactModification() {
-        Contacts before = app.contact().all();
+        Contacts before = app.db().contacts();
         ContactData modifiedContact = before.iterator().next();
         ContactData contact = new ContactData().withId(modifiedContact.getId()).withName("Денис").withMiddleName("Станиславович").withLastName("Воронцов")
                 .withNickName("DEX-6").withCompany("Космический Мозгоед").withAddress("планета Земля")
                 .withPhone("+79856405255").withEmail("dex-6@mail.ru");
+        app.goTo().homePage();
         app.contact().modify(contact);
         Assert.assertEquals(app.contact().count(), before.size());
-        Contacts after = app.contact().all();
-        assertThat(after, equalTo(before.without(modifiedContact).withAdded(modifiedContact)));
+        Contacts after = app.db().contacts();
+        assertThat(after, equalTo(before.without(modifiedContact).withAdded(contact)));
     }
 }
