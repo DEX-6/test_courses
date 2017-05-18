cbf5b2f9e97fb282312a371ba443fc392c2fe73f
DEX-6 <irina14.91@bk.ru>
2017-05-18 11:08:16 +0300

    методы verify

--- addressbook-web-tests/src/test/java/ru/stqa/test_courses/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/test_courses/addressbook/tests/TestBase.java
@@ -4,6 +4,14 @@ import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.test_courses.addressbook.appmeneger.ApplicationManager;
+import ru.stqa.test_courses.addressbook.model.Contacts;
+import ru.stqa.test_courses.addressbook.model.GroupData;
+import ru.stqa.test_courses.addressbook.model.Groups;
+
+import java.util.stream.Collectors;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
  * Created by i-ru on 09.02.2017.
@@ -22,5 +30,23 @@ public class TestBase {
     @AfterSuite
     public void tearDown() {
         app.stop();
+    }
+
+    public void verifyGroupsListInUI() {
+        if(Boolean.getBoolean("verifyUI")) {
+            Groups dbGroups = app.db().groups();
+            Groups uiGroups = app.group().all();
+            assertThat(uiGroups, equalTo(dbGroups.stream()
+                    .map((g) -> new GroupData().withId(g.getId()).withName(g.getName()))
+                    .collect(Collectors.toSet())));
+        }
+    }
+
+    public void verifyContactsListInUI() {
+        if(Boolean.getBoolean("verifyUI")) {
+            Contacts dbContacts = app.db().contacts();
+            Contacts uiContacts = app.contact().all();
+            assertThat(uiContacts, equalTo(dbContacts));
+        }
     }
 }
