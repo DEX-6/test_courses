58d2a8e1ba7a812c84afd229d610cc50f4cdfa95
DEX-6 <irina14.91@bk.ru>
2018-08-05 16:46:23 +0300

    Добавлен новый модуль для тестирования mantis + страные изменнения

---
+++ sandbox/src/main/java/ru/stqa/test_courses/sandbox/Rectangle.java
@@ -0,0 +1,18 @@
+package ru.stqa.test_courses.sandbox;
+
+/**
+ * Created by i-ru on 29.01.2017.
+ */
+public class Rectangle {
+    public double a;
+    public double b;
+
+    public Rectangle(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public double area() {
+        return this.a * this.b;
+    }
+}
---
+++ sandbox/src/main/java/ru/stqa/test_courses/sandbox/Square.java
@@ -0,0 +1,17 @@
+package ru.stqa.test_courses.sandbox;
+
+/**
+ * Created by i-ru on 29.01.2017.
+ */
+public class Square {
+
+    public double l;
+
+    public Square(double l) {
+        this.l = l;
+    }
+
+    public double area() {
+        return this.l * this.l;
+    }
+}
